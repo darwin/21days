7339f0b5b178ae4c6cfd169eea0d0aad89a2e166
Antonin Hildebrand <antonin@hildebrand.cz>
2011-02-12 18:54:52 +0100

    nejaky progress

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -21,27 +21,35 @@ $(function() {
     window.currentUserId = null;
     
     Date.prototype.getD = function() {
-        return Math.floor(this.valueOf() / 1000*60*60*24);
+        return Math.floor(this.valueOf() / (1000*60*60*24));
     }
     
     window.User = Backbone.Model.extend({
         localStorage: new Store("user"),
         
         initialize: function() {
-            _.bindAll(this, 'addOne', 'addAll');
+            _.bindAll(this, 'addOne', 'addAll', 'resave');
+            
+            if (!this.get("start_day")) {
+                this.set({
+                    "start_day": new Date().getD()
+                });
+            }
             
             this.routines = new RoutineList;
             this.routines.url = "/users/"+this.id+"/routines";
 
             this.routines.bind('add', this.addOne);
             this.routines.bind('refresh', this.addAll);
+            this.routines.bind('change', this.resave);
 
             this.routines.fetch();
         },
 
         addOne: function(routine) {
             var view = new RoutineView({
-                model: routine
+                model: routine,
+                firstDay: this.get('start_day')
             });
             $("#routine-list").append(view.render().el);
         },
@@ -50,6 +58,9 @@ $(function() {
             this.routines.each(this.addOne);
         },
 
+        resave: function() {
+            this.save({'routines': this.routines.toJSON()});
+        }
 
     });
     
@@ -67,9 +78,9 @@ $(function() {
                     "frequency": 1
                 });
             }
-            if (!this.get("checkings")) {
+            if (!this.get("checkins")) {
                 this.set({
-                    "checkings": {}
+                    "checkins": {}
                 });
             }
         },
@@ -77,6 +88,31 @@ $(function() {
         toggle: function() {
             this.save({
                 done: !this.get("done")
+            });
+        },
+        
+        isDayChecked: function(dindex) {
+            var checkins = this.get('checkins');
+            if (checkins) {
+                var record = checkins[dindex];
+                if (record && record.checked) return true;
+            }
+            return false;
+        },
+        
+        toggleDayCheckin: function(dindex) {
+            var checkins = this.get('checkins') || {};
+            checkins = _.clone(checkins);
+            var record = checkins[dindex];
+            if (!record) {
+                checkins[dindex] = {
+                    checked: true
+                };
+            } else {
+                checkins[dindex].checked = !checkins[dindex].checked;
+            }
+            this.set({
+                'checkins': checkins
             });
         },
 
@@ -111,62 +147,55 @@ $(function() {
     });
 
     window.DayView = Backbone.View.extend({
-        template: _.template($('#week-template').html()),
+        className: 'day',
+        template: _.template($('#day-template').html()),
         events: {
-            "click .check": "toggleDone",
-            "dblclick div.routine-name": "edit",
-            "click span.routine-destroy": "clear",
-            "keypress .routine-input": "updateOnEnter"
+            "click": "check",
         },
 
         initialize: function() {
-            _.bindAll(this, 'render', 'close');
-            this.model.bind('change', this.render);
+            _.bindAll(this, 'render', 'check');
         },
 
         render: function() {
-            // $(this.el).html(this.template(this.model.toJSON()));
-            // this.setContent();
+            $(this.el).html(this.template(this.model.toJSON()));
+            if (this.model.isDayChecked(this.options.dindex)) $(this.el).addClass('checked');
             return this;
         },
 
-        setContent: function() {
-            // var name = this.model.get('name');
-            // this.$('.routine-name').text(name);
-            // this.$input = this.$('.routine-input');
-            // this.$input.bind('blur', this.close);
-            // this.$input.val(name);
+        check: function() {
+            console.log('checked', this, this.options.dindex);
+            this.model.toggleDayCheckin(this.options.dindex);
+            this.model.save();
         }
 
     });
 
     window.WeekView = Backbone.View.extend({
+        className: 'week',
         template: _.template($('#week-template').html()),
         events: {
-            "click .check": "toggleDone",
-            "dblclick div.routine-name": "edit",
-            "click span.routine-destroy": "clear",
-            "keypress .routine-input": "updateOnEnter"
         },
 
         initialize: function() {
             _.bindAll(this, 'render');
-            this.model.bind('change', this.render);
         },
 
         render: function() {
             $(this.el).html(this.template(this.model.toJSON()));
+            
+            var root = $(this.el);
+            for (var i=0; i<7; i++) {
+                var day = new DayView({
+                    model: this.model,
+                    dindex: this.options.dindex + i
+                });
+                day.render();
+                root.append(day.el);
+            }
+            
             return this;
         },
-
-        setContent: function() {
-            // var name = this.model.get('name');
-            // this.$('.routine-name').text(name);
-            // this.$input = this.$('.routine-input');
-            // this.$input.bind('blur', this.close);
-            // this.$input.val(name);
-        },
-
     });
 
     window.RoutineView = Backbone.View.extend({
@@ -190,13 +219,16 @@ $(function() {
             this.setContent();
             
             var week1 = new WeekView({
-                model: this.model
+                model: this.model,
+                dindex: this.options.firstDay
             });
             var week2 = new WeekView({
-                model: this.model
+                model: this.model,
+                dindex: this.options.firstDay + 7
             });
             var week3 = new WeekView({
-                model: this.model
+                model: this.model,
+                dindex: this.options.firstDay + 14
             });
             
             week1.render();
@@ -254,7 +286,6 @@ $(function() {
         events: {
             "keypress #new-routine": "createOnEnter",
             "keypress #new-routine-frequency": "createOnEnter",
-            // "keyup #new-routine": "showTooltip",
             "click .routine-clear a": "clearCompleted"
         },
 
