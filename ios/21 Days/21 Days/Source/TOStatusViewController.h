06b7dc684bb70e868ce65886d736dbb50c08d62c
Zdenek Nemec <zdenek@rwy9.com>
2011-02-12 13:13:57 +0100

    Source reogranization, Views origin.

---
+++ ios/21 Days/21 Days/Source/TOStatusViewController.h
@@ -0,0 +1,16 @@
+//
+//  TOStatusViewController.h
+//  21 Days
+//
+//  Created by Zdenek on 2/12/11.
+//  Copyright 2011 EskoArtwork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TOStatusViewController : UIViewController {
+    
+}
+
+@end
