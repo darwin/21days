f181e1b0f1d59fe4efc4c4dcb8b2ddaa0fe7e5cc
Antonin Hildebrand <antonin@hildebrand.cz>
2011-02-12 17:06:42 +0100

    mezikomit

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,5 +1,58 @@
-$(function() {
+//
+// window.App
+// window.User
+// 
+// User has many Routines
+// Routines is collection of Routine
+//
+// User
+//  - id
+//  - facebook (info)
+//  - start_day (dindex)
+//
+// Routine
+//  - name
+//  - checkins (hash of jsons, indexed by dindex)
+//
+// Today (dindex)
 
+$(function() {
+    
+    window.currentUserId = null;
+    
+    Date.prototype.getD = function() {
+        return Math.floor(this.valueOf() / 1000*60*60*24);
+    }
+    
+    window.User = Backbone.Model.extend({
+        localStorage: new Store("user"),
+        
+        initialize: function() {
+            _.bindAll(this, 'addOne', 'addAll');
+            
+            this.routines = new RoutineList;
+            this.routines.url = "/users/"+this.id+"/routines";
+
+            this.routines.bind('add', this.addOne);
+            this.routines.bind('refresh', this.addAll);
+
+            this.routines.fetch();
+        },
+
+        addOne: function(routine) {
+            var view = new RoutineView({
+                model: routine
+            });
+            $("#routine-list").append(view.render().el);
+        },
+
+        addAll: function() {
+            this.routines.each(this.addOne);
+        },
+
+
+    });
+    
     window.Routine = Backbone.Model.extend({
         EMPTY: "define your routine...",
 
@@ -12,6 +65,11 @@ $(function() {
             if (!this.get("frequency")) {
                 this.set({
                     "frequency": 1
+                });
+            }
+            if (!this.get("checkings")) {
+                this.set({
+                    "checkings": {}
                 });
             }
         },
@@ -52,7 +110,64 @@ $(function() {
         }
     });
 
-    window.Routines = new RoutineList;
+    window.DayView = Backbone.View.extend({
+        template: _.template($('#week-template').html()),
+        events: {
+            "click .check": "toggleDone",
+            "dblclick div.routine-name": "edit",
+            "click span.routine-destroy": "clear",
+            "keypress .routine-input": "updateOnEnter"
+        },
+
+        initialize: function() {
+            _.bindAll(this, 'render', 'close');
+            this.model.bind('change', this.render);
+        },
+
+        render: function() {
+            // $(this.el).html(this.template(this.model.toJSON()));
+            // this.setContent();
+            return this;
+        },
+
+        setContent: function() {
+            // var name = this.model.get('name');
+            // this.$('.routine-name').text(name);
+            // this.$input = this.$('.routine-input');
+            // this.$input.bind('blur', this.close);
+            // this.$input.val(name);
+        }
+
+    });
+
+    window.WeekView = Backbone.View.extend({
+        template: _.template($('#week-template').html()),
+        events: {
+            "click .check": "toggleDone",
+            "dblclick div.routine-name": "edit",
+            "click span.routine-destroy": "clear",
+            "keypress .routine-input": "updateOnEnter"
+        },
+
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.model.bind('change', this.render);
+        },
+
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        setContent: function() {
+            // var name = this.model.get('name');
+            // this.$('.routine-name').text(name);
+            // this.$input = this.$('.routine-input');
+            // this.$input.bind('blur', this.close);
+            // this.$input.val(name);
+        },
+
+    });
 
     window.RoutineView = Backbone.View.extend({
         tagName: "li",
@@ -73,6 +188,23 @@ $(function() {
         render: function() {
             $(this.el).html(this.template(this.model.toJSON()));
             this.setContent();
+            
+            var week1 = new WeekView({
+                model: this.model
+            });
+            var week2 = new WeekView({
+                model: this.model
+            });
+            var week3 = new WeekView({
+                model: this.model
+            });
+            
+            week1.render();
+            week2.render();
+            week3.render();
+            
+            this.$('.routine-weeks').empty().append(week1.el, week2.el, week3.el);
+            
             return this;
         },
 
@@ -82,6 +214,11 @@ $(function() {
             this.$input = this.$('.routine-input');
             this.$input.bind('blur', this.close);
             this.$input.val(name);
+        },
+        
+        close: function() {
+            this.model.save({name: this.$input.val()});
+            $(this.el).removeClass("editing");
         },
 
         // Toggle the `"done"` state of the model.
@@ -93,13 +230,6 @@ $(function() {
         edit: function() {
             $(this.el).addClass("editing");
             this.$input.focus();
-        },
-
-        close: function() {
-            this.model.save({
-                name: this.$input.val()
-            });
-            $(this.el).removeClass("editing");
         },
 
         // If you hit `enter`, we're through editing the item.
@@ -124,78 +254,71 @@ $(function() {
         events: {
             "keypress #new-routine": "createOnEnter",
             "keypress #new-routine-frequency": "createOnEnter",
-            "keyup #new-routine": "showTooltip",
+            // "keyup #new-routine": "showTooltip",
             "click .routine-clear a": "clearCompleted"
         },
 
         initialize: function() {
-            _.bindAll(this, 'addOne', 'addAll', 'render');
 
+            _.bindAll(this, 'render');
+            
             this.$input = this.$("#new-routine");
             this.$frequency = this.$("#new-routine-frequency");
-
-            Routines.bind('add', this.addOne);
-            Routines.bind('refresh', this.addAll);
-            Routines.bind('all', this.render);
-
-            Routines.fetch();
+            
+            var date = new Date();
+            
+            this.today = date.getD();
+            this.user = window.User;
+            this.user.routines.bind('all', this.render);
+            this.user.bind('all', this.render);
         },
 
         render: function() {
-            var done = Routines.done().length;
+            var done = this.user.routines.done().length;
             this.$('#stats').html(this.statsTemplate({
-                total: Routines.length,
-                done: Routines.done().length,
-                remaining: Routines.remaining().length
+                total: this.user.routines.length,
+                done: this.user.routines.done().length,
+                remaining: this.user.routines.remaining().length
             }));
-        },
-
-        addOne: function(routine) {
-            var view = new RoutineView({
-                model: routine
-            });
-            this.$("#routine-list").append(view.render().el);
-        },
-
-        addAll: function() {
-            Routines.each(this.addOne);
         },
 
         newAttributes: function() {
             return {
                 name: this.$input.val(),
                 frequency: parseInt(this.$frequency.val().substring(1), 10), //  f1, f3 or f7
-                order: Routines.nextOrder(),
+                order: this.user.routines.nextOrder(),
                 done: false
             };
         },
 
         createOnEnter: function(e) {
             if (e.keyCode != 13) return;
-            Routines.create(this.newAttributes());
+            this.user.routines.create(this.newAttributes());
             this.$input.val('');
         },
 
         clearCompleted: function() {
-            _.each(Routines.done(), function(routine) {
+            _.each(this.user.routines.done(), function(routine) {
                 routine.clear();
             });
             return false;
         },
 
         // Lazily show the tooltip that tells you to press `enter` to save a new day item, after one second.
-        showTooltip: function(e) {
-            var tooltip = this.$(".ui-tooltip-top");
-            var val = this.$input.val();
-            tooltip.fadeOut();
-            if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-            if (val == '' || val == this.$input.attr('placeholder')) return;
-            var show = function() {
-                tooltip.show().fadeIn();
-            };
-            this.tooltipTimeout = _.delay(show, 1000);
-        }
+        // showTooltip: function(e) {
+        //     var tooltip = this.$(".ui-tooltip-top");
+        //     var val = this.$input.val();
+        //     tooltip.fadeOut();
+        //     if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
+        //     if (val == '' || val == this.$input.attr('placeholder')) return;
+        //     var show = function() {
+        //         tooltip.show().fadeIn();
+        //     };
+        //     this.tooltipTimeout = _.delay(show, 1000);
+        // }
     });
 
+    window.Today = new Date().getD();
+    window.User = new User();
     window.App = new AppView;
 });
