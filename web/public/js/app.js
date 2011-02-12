d64da9649d3b5ab1fcdf913353772ce0b9cf360d
Antonin Hildebrand <antonin@hildebrand.cz>
2011-02-12 11:05:26 +0100

    rebranding

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,256 +1,261 @@
-// An example Backbone application contributed by
-// [JÃ©rÃ´me Gravel-Niquet](http://jgn.me/). This demo uses a simple
-// [LocalStorage adapter](backbone-localstorage.html)
-// to persist Backbone models within your browser.
-
 // Load the application once the DOM is ready, using `jQuery.ready`:
-$(function(){
+$(function() {
 
-  // Todo Model
-  // ----------
+    // Day Model
+    // ----------
+    // Our basic **Day** model has `content`, `order`, and `done` attributes.
+    window.Day = Backbone.Model.extend({
 
-  // Our basic **Todo** model has `content`, `order`, and `done` attributes.
-  window.Todo = Backbone.Model.extend({
+        // If you don't provide a day, one will be provided for you.
+        EMPTY: "empty day...",
 
-    // If you don't provide a todo, one will be provided for you.
-    EMPTY: "empty todo...",
+        // Ensure that each day created has `content`.
+        initialize: function() {
+            if (!this.get("content")) {
+                this.set({
+                    "content": this.EMPTY
+                });
+            }
+        },
 
-    // Ensure that each todo created has `content`.
-    initialize: function() {
-      if (!this.get("content")) {
-        this.set({"content": this.EMPTY});
-      }
-    },
+        // Toggle the `done` state of this day item.
+        toggle: function() {
+            this.save({
+                done: !this.get("done")
+            });
+        },
 
-    // Toggle the `done` state of this todo item.
-    toggle: function() {
-      this.save({done: !this.get("done")});
-    },
+        // Remove this Day from *localStorage* and delete its view.
+        clear: function() {
+            this.destroy();
+            this.view.remove();
+        }
 
-    // Remove this Todo from *localStorage* and delete its view.
-    clear: function() {
-      this.destroy();
-      this.view.remove();
-    }
+    });
 
-  });
+    // Day Collection
+    // ---------------
+    // The collection of days is backed by *localStorage* instead of a remote
+    // server.
+    window.DayList = Backbone.Collection.extend({
 
-  // Todo Collection
-  // ---------------
+        // Reference to this collection's model.
+        model: Day,
 
-  // The collection of todos is backed by *localStorage* instead of a remote
-  // server.
-  window.TodoList = Backbone.Collection.extend({
+        // Save all of the day items under the `"days"` namespace.
+        localStorage: new Store("days"),
 
-    // Reference to this collection's model.
-    model: Todo,
+        // Filter down the list of all day items that are finished.
+        done: function() {
+            return this.filter(function(day) {
+                return day.get('done');
+            });
+        },
 
-    // Save all of the todo items under the `"todos"` namespace.
-    localStorage: new Store("todos"),
+        // Filter down the list to only day items that are still not finished.
+        remaining: function() {
+            return this.without.apply(this, this.done());
+        },
 
-    // Filter down the list of all todo items that are finished.
-    done: function() {
-      return this.filter(function(todo){ return todo.get('done'); });
-    },
+        // We keep the Days in sequential order, despite being saved by unordered
+        // GUID in the database. This generates the next order number for new items.
+        nextOrder: function() {
+            if (!this.length) return 1;
+            return this.last().get('order') + 1;
+        },
 
-    // Filter down the list to only todo items that are still not finished.
-    remaining: function() {
-      return this.without.apply(this, this.done());
-    },
+        // Days are sorted by their original insertion order.
+        comparator: function(day) {
+            return day.get('order');
+        }
 
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
-    nextOrder: function() {
-      if (!this.length) return 1;
-      return this.last().get('order') + 1;
-    },
+    });
 
-    // Todos are sorted by their original insertion order.
-    comparator: function(todo) {
-      return todo.get('order');
-    }
+    // Create our global collection of **Days**.
+    window.Days = new DayList;
 
-  });
+    // Day Item View
+    // --------------
+    // The DOM element for a day item...
+    window.DayView = Backbone.View.extend({
 
-  // Create our global collection of **Todos**.
-  window.Todos = new TodoList;
+        //... is a list tag.
+        tagName: "li",
 
-  // Todo Item View
-  // --------------
+        // Cache the template function for a single item.
+        template: _.template($('#item-template').html()),
 
-  // The DOM element for a todo item...
-  window.TodoView = Backbone.View.extend({
+        // The DOM events specific to an item.
+        events: {
+            "click .check": "toggleDone",
+            "dblclick div.day-content": "edit",
+            "click span.day-destroy": "clear",
+            "keypress .day-input": "updateOnEnter"
+        },
 
-    //... is a list tag.
-    tagName:  "li",
+        // The DayView listens for changes to its model, re-rendering. Since there's
+        // a one-to-one correspondence between a **Day** and a **DayView** in this
+        // app, we set a direct reference on the model for convenience.
+        initialize: function() {
+            _.bindAll(this, 'render', 'close');
+            this.model.bind('change', this.render);
+            this.model.view = this;
+        },
 
-    // Cache the template function for a single item.
-    template: _.template($('#item-template').html()),
+        // Re-render the contents of the day item.
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            this.setContent();
+            return this;
+        },
 
-    // The DOM events specific to an item.
-    events: {
-      "click .check"              : "toggleDone",
-      "dblclick div.todo-content" : "edit",
-      "click span.todo-destroy"   : "clear",
-      "keypress .todo-input"      : "updateOnEnter"
-    },
+        // To avoid XSS (not that it would be harmful in this particular app),
+        // we use `jQuery.text` to set the contents of the day item.
+        setContent: function() {
+            var content = this.model.get('content');
+            this.$('.day-content').text(content);
+            this.input = this.$('.day-input');
+            this.input.bind('blur', this.close);
+            this.input.val(content);
+        },
 
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-    // app, we set a direct reference on the model for convenience.
-    initialize: function() {
-      _.bindAll(this, 'render', 'close');
-      this.model.bind('change', this.render);
-      this.model.view = this;
-    },
+        // Toggle the `"done"` state of the model.
+        toggleDone: function() {
+            this.model.toggle();
+        },
 
-    // Re-render the contents of the todo item.
-    render: function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      this.setContent();
-      return this;
-    },
+        // Switch this view into `"editing"` mode, displaying the input field.
+        edit: function() {
+            $(this.el).addClass("editing");
+            this.input.focus();
+        },
 
-    // To avoid XSS (not that it would be harmful in this particular app),
-    // we use `jQuery.text` to set the contents of the todo item.
-    setContent: function() {
-      var content = this.model.get('content');
-      this.$('.todo-content').text(content);
-      this.input = this.$('.todo-input');
-      this.input.bind('blur', this.close);
-      this.input.val(content);
-    },
+        // Close the `"editing"` mode, saving changes to the day.
+        close: function() {
+            this.model.save({
+                content: this.input.val()
+            });
+            $(this.el).removeClass("editing");
+        },
 
-    // Toggle the `"done"` state of the model.
-    toggleDone: function() {
-      this.model.toggle();
-    },
+        // If you hit `enter`, we're through editing the item.
+        updateOnEnter: function(e) {
+            if (e.keyCode == 13) this.close();
+        },
 
-    // Switch this view into `"editing"` mode, displaying the input field.
-    edit: function() {
-      $(this.el).addClass("editing");
-      this.input.focus();
-    },
+        // Remove this view from the DOM.
+        remove: function() {
+            $(this.el).remove();
+        },
 
-    // Close the `"editing"` mode, saving changes to the todo.
-    close: function() {
-      this.model.save({content: this.input.val()});
-      $(this.el).removeClass("editing");
-    },
+        // Remove the item, destroy the model.
+        clear: function() {
+            this.model.clear();
+        }
 
-    // If you hit `enter`, we're through editing the item.
-    updateOnEnter: function(e) {
-      if (e.keyCode == 13) this.close();
-    },
+    });
 
-    // Remove this view from the DOM.
-    remove: function() {
-      $(this.el).remove();
-    },
+    // The Application
+    // ---------------
+    // Our overall **AppView** is the top-level piece of UI.
+    window.AppView = Backbone.View.extend({
 
-    // Remove the item, destroy the model.
-    clear: function() {
-      this.model.clear();
-    }
+        // Instead of generating a new element, bind to the existing skeleton of
+        // the App already present in the HTML.
+        el: $("#dayapp"),
 
-  });
+        // Our template for the line of statistics at the bottom of the app.
+        statsTemplate: _.template($('#stats-template').html()),
 
-  // The Application
-  // ---------------
+        // Delegated events for creating new items, and clearing completed ones.
+        events: {
+            "keypress #new-day": "createOnEnter",
+            "keyup #new-day": "showTooltip",
+            "click .day-clear a": "clearCompleted"
+        },
 
-  // Our overall **AppView** is the top-level piece of UI.
-  window.AppView = Backbone.View.extend({
+        // At initialization we bind to the relevant events on the `Days`
+        // collection, when items are added or changed. Kick things off by
+        // loading any preexisting days that might be saved in *localStorage*.
+        initialize: function() {
+            _.bindAll(this, 'addOne', 'addAll', 'render');
 
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
-    el: $("#todoapp"),
+            this.input = this.$("#new-day");
 
-    // Our template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
+            Days.bind('add', this.addOne);
+            Days.bind('refresh', this.addAll);
+            Days.bind('all', this.render);
 
-    // Delegated events for creating new items, and clearing completed ones.
-    events: {
-      "keypress #new-todo":  "createOnEnter",
-      "keyup #new-todo":     "showTooltip",
-      "click .todo-clear a": "clearCompleted"
-    },
+            Days.fetch();
+        },
 
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
-    initialize: function() {
-      _.bindAll(this, 'addOne', 'addAll', 'render');
+        // Re-rendering the App just means refreshing the statistics -- the rest
+        // of the app doesn't change.
+        render: function() {
+            var done = Days.done().length;
+            this.$('#day-stats').html(this.statsTemplate({
+                total: Days.length,
+                done: Days.done().length,
+                remaining: Days.remaining().length
+            }));
+        },
 
-      this.input    = this.$("#new-todo");
+        // Add a single day item to the list by creating a view for it, and
+        // appending its element to the `<ul>`.
+        addOne: function(day) {
+            var view = new DayView({
+                model: day
+            });
+            this.$("#day-list").append(view.render().el);
+        },
 
-      Todos.bind('add',     this.addOne);
-      Todos.bind('refresh', this.addAll);
-      Todos.bind('all',     this.render);
+        // Add all items in the **Days** collection at once.
+        addAll: function() {
+            Days.each(this.addOne);
+        },
 
-      Todos.fetch();
-    },
+        // Generate the attributes for a new Day item.
+        newAttributes: function() {
+            return {
+                content: this.input.val(),
+                order: Days.nextOrder(),
+                done: false
+            };
+        },
 
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
-    render: function() {
-      var done = Todos.done().length;
-      this.$('#todo-stats').html(this.statsTemplate({
-        total:      Todos.length,
-        done:       Todos.done().length,
-        remaining:  Todos.remaining().length
-      }));
-    },
+        // If you hit return in the main input field, create new **Day** model,
+        // persisting it to *localStorage*.
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            Days.create(this.newAttributes());
+            this.input.val('');
+        },
 
-    // Add a single todo item to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
-    addOne: function(todo) {
-      var view = new TodoView({model: todo});
-      this.$("#todo-list").append(view.render().el);
-    },
+        // Clear all done day items, destroying their models.
+        clearCompleted: function() {
+            _.each(Days.done(), function(day) {
+                day.clear();
+            });
+            return false;
+        },
 
-    // Add all items in the **Todos** collection at once.
-    addAll: function() {
-      Todos.each(this.addOne);
-    },
+        // Lazily show the tooltip that tells you to press `enter` to save
+        // a new day item, after one second.
+        showTooltip: function(e) {
+            var tooltip = this.$(".ui-tooltip-top");
+            var val = this.input.val();
+            tooltip.fadeOut();
+            if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
+            if (val == '' || val == this.input.attr('placeholder')) return;
+            var show = function() {
+                tooltip.show().fadeIn();
+            };
+            this.tooltipTimeout = _.delay(show, 1000);
+        }
 
-    // Generate the attributes for a new Todo item.
-    newAttributes: function() {
-      return {
-        content: this.input.val(),
-        order:   Todos.nextOrder(),
-        done:    false
-      };
-    },
+    });
 
-    // If you hit return in the main input field, create new **Todo** model,
-    // persisting it to *localStorage*.
-    createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-      Todos.create(this.newAttributes());
-      this.input.val('');
-    },
-
-    // Clear all done todo items, destroying their models.
-    clearCompleted: function() {
-      _.each(Todos.done(), function(todo){ todo.clear(); });
-      return false;
-    },
-
-    // Lazily show the tooltip that tells you to press `enter` to save
-    // a new todo item, after one second.
-    showTooltip: function(e) {
-      var tooltip = this.$(".ui-tooltip-top");
-      var val = this.input.val();
-      tooltip.fadeOut();
-      if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-      if (val == '' || val == this.input.attr('placeholder')) return;
-      var show = function(){ tooltip.show().fadeIn(); };
-      this.tooltipTimeout = _.delay(show, 1000);
-    }
-
-  });
-
-  // Finally, we kick things off by creating the **App**.
-  window.App = new AppView;
+    // Finally, we kick things off by creating the **App**.
+    window.App = new AppView;
 
 });
