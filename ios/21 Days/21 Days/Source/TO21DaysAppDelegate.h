71c003db106958bd0853d5f6bb5262fe7f26f3d1
Zdenek Nemec <zdenek@rwy9.com>
2011-02-12 13:15:34 +0100

    view nibs added

---
+++ ios/21 Days/21 Days/Source/TO21DaysAppDelegate.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface TO21DaysAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
