0651d3a177d003ad0665c66400054319bbe94b2f
Miroslav Varga <k_et_m@yahoo.com>
2011-02-13 10:59:31 +0100

    email notification skeleton

--- web/app/jobs/Notifier.java
+++ web/app/jobs/Notifier.java
@@ -4,6 +4,14 @@ import models.*;
 import notifiers.*;
 import play.jobs.*;
 
+// TODO jeden den pred zaciatkom aktivity poslat mail ze zajtra zacina
+// TODO ak ma ulohu kazdych 7 dni tak dva dni po tom co mal mat splnenu
+// ulohu poslat mail
+//TODO ak ma ulohu kazdych 3 dni tak dva dni po tom co mal mat splnenu
+//ulohu poslat mail
+//TODO ak ma ulohu kazdych 7 dni tak dva dni po tom co mal mat splnenu
+//ulohu poslat mail
+
 @Every("24h")
 public class Notifier extends Job<Void> {
     @Override
